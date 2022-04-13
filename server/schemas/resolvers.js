f689032beb0855f29d1f3fbfeb6794accc67a403
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-12 19:29:13 -0500

    added mutations to the resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,25 +3,33 @@ const { User, Thought } = require("../models");
 const resolvers = {
   Query: {
     users: async () => {
-        return User.find()
-          .select('-__v -password')
-          .populate('thoughts')
-          .populate('friends');
+      return User.find()
+        .select("-__v -password")
+        .populate("thoughts")
+        .populate("friends");
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+
+        return user;
       },
-      user: async (parent, { username }) => {
-        return User.findOne({ username })
-          .select('-__v -password')
-          .populate('friends')
-          .populate('thoughts');
-      },
-      thoughts: async (parent, { username }) => {
-        const params = username ? { username } : {};
-        return Thought.find(params).sort({ createdAt: -1 });
-      },
-      thought: async (parent, { _id }) => {
-        return Thought.findOne({ _id });
-      }
-    }
+      login: async () => {},
+    },
+  },
 };
 
 module.exports = resolvers;
